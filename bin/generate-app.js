d3b2f8fcc03f8c711d63814faf53056d82852265
dmquinn <danielmurphyberling@gmail.com>
2021-12-20 16:14:42 +0100

    changes

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -12,7 +12,7 @@ const runCommand = (command) => {
 };
 
 const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/dmquinn/create-dmq-app ${repoName}`;
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/dmquinn/mern-boilerplate ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
 console.log(`Cloning the repository with name ${repoName}`);
