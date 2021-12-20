8267d5508e4f3afd7b07d1d0d9f7532383edc28b
dmquinn <danielmurphyberling@gmail.com>
2021-12-20 12:24:37 +0100

    dev dependency issues

--- generate-app.js
+++ generate-app.js
@@ -4,9 +4,10 @@ const fs = require("fs");
 if (process.argv.length < 3) {
   console.log("You have to provide a name to your app.");
   console.log("For example :");
-  console.log("    npx create-dmq-app my-app");
+  console.log("npx create-dmq-app my-app");
   process.exit(1);
-}const projectName = process.argv[2];
+}
+const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
 const git_repo = "https://github.com/dmquinn/mern-boilerplate.git";
@@ -18,28 +19,27 @@ try {
       `The file ${projectName} already exist in the current directory, please give it another name.`
     );
   } else {
-    console.log(error);
+    console.log(err);
   }
   process.exit(1);
 }
 async function main() {
-    try {
-      console.log('Downloading files...');
-      execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
+  try {
+    console.log("Downloading files...");
+    execSync(`git clone --depth 1 ${git_repo} ${projectPath}`);
 
-      process.chdir(projectPath);
+    process.chdir(projectPath);
 
-      console.log('Installing dependencies...');
-      execSync('npm install');
+    console.log("Installing dependencies...");
+    execSync("npm install");
 
-      console.log('Removing useless files);
-      execSync('npx rimraf ./.git');
-      fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true});
+    console.log("Removing useless files");
+    execSync("npx rimraf ./.git");
+    fs.rmdirSync(path.join(projectPath, "bin"), { recursive: true });
 
-      console.log('The installation is done, Lets get coding!!');
-
-    } catch (error) {
-      console.log(error);
-    }
+    console.log("The installation is done, Lets get coding!!");
+  } catch (error) {
+    console.log(error);
+  }
 }
 main();
